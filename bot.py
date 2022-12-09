33e4dce926fed5d22bcece9e9f4b901b175d8915
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-09 16:13:04 +0330

    dd

--- bot.py
+++ bot.py
@@ -18,21 +18,6 @@ SEND_MESSAGE = 1
 # Define a function that prompts the user to enter a message
 def send_message(bot, update):
     # Log the message that the bot received
-    # logging.info("Received message: %s" % update.message.text)
-
-    # Create a keyboard with a single button to submit the message
-    keyboard = [['Send message']]
-    reply_markup = ReplyKeyboardMarkup(keyboard)
-
-    # Prompt the user to enter a message
-    update.message.reply_text("Please enter a message for the chatbot:", reply_markup=reply_markup)
-
-    # Move to the next conversation state
-    return SEND_MESSAGE
-
-# Define a function that sends the user's message to the chatbot and displays the response
-def send_message(bot, update):
-    # Log the message that the bot received
     logging.info("Received message: %s" % update.effective_message.text)
 
     # Create a keyboard with a single button to submit the message
@@ -44,6 +29,32 @@ def send_message(bot, update):
 
     # Move to the next conversation state
     return SEND_MESSAGE
+
+# Define a function that sends the user's message to the chatbot and displays the response
+def send_message_to_chatbot(bot, update):
+    
+    # Get the user's message
+    message = update.message.text
+
+    # Log the message that the user sent to the bot
+    logging.info("Sending message to chatbot: %s" % message)
+
+    # Use the OpenAI API to get a response from the chatbot
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=1024,
+        temperature=0.5,
+    )
+
+    # Extract the response from the API response
+    response_text = response["choices"][0]["text"]
+
+    # Send the response back to the user
+    update.message.reply_text(response_text)
+
+    # End the conversation
+    return ConversationHandler.END
 
 # Set up the Telegram bot to respond to the "/chatbot" command
 updater = Updater("5776863971:AAERKuZgOuu0MtehJ1veynxTz741G7sS5nY")
--- echo.py
+++ echo.py
@@ -1,27 +1,16 @@
-import telegram
-from telegram.ext import Updater, CommandHandler
+import openai
+openai.api_key = "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"
 
-# Replace "YOUR_API_TOKEN" with your API token
-bot = telegram.Bot(token="5776863971:AAERKuZgOuu0MtehJ1veynxTz741G7sS5nY")
+def chatbot(prompt):
+  response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+  )
 
-updater = Updater(bot=bot)
+  return response["choices"][0]["text"]
 
-# This function will be called whenever the user sends a message to the bot
-def echo(update, context):
-    # Get the user's message
-    message = update.message.text
-    
-    # Convert the message to uppercase
-    message = message.upper()
-    
-    # Send the message back to the user
-    update.message.reply_text(message)
-
-# This command handler will respond to the user's "/start" message
-start_handler = CommandHandler("start", echo)
-
-# Add the command handler to the updater
-updater.dispatcher.add_handler(start_handler)
-
-# Start the updater
-updater.start_polling()
+print(chatbot("Hello, how are you today?"))
