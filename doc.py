e11fda44a1877d1874b034e5ba3c9655464e4288
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-11 14:16:37 +0000

    Dec 11, 2022, 5:46 PM

--- doc.py
+++ doc.py
@@ -91,6 +91,10 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    # Flip the image horizontally for a later selfie-view display
+    image = cv2.flip(image, 1)
+    
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         # Calculate the angle between the index finger and thumb.
