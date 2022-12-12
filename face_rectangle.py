39602edc7b5222d78ff86119cca8237475f9c508
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-12 17:54:40 +0000

    Dec 12, 2022, 9:24 PM

--- face_rectangle.py
+++ face_rectangle.py
@@ -1,21 +1,22 @@
 import cv2
+import numpy as np
 
 # Create a cascade classifier object
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_cascade: cv2.CascadeClassifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Open the default camera
-cap = cv2.VideoCapture(0)
+cap: cv2.VideoCapture = cv2.VideoCapture(0)
 
 # Loop until the user hits the 'Esc' key
 while True:
     # Read the next frame from the camera
-    ret, frame = cap.read()
+    ret, frame: np.ndarray = cap.read()
 
     # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray: np.ndarray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(gray)
+    faces: np.ndarray = face_cascade.detectMultiScale(gray)
 
     # Draw a rectangle around each face
     for (x, y, w, h) in faces:
@@ -25,7 +26,7 @@ while True:
     cv2.imshow("Face Detection", frame)
 
     # Check if the user hit the 'Esc' key
-    c = cv2.waitKey(1)
+    c: int = cv2.waitKey(1)
     if c == 27:
         break
 
