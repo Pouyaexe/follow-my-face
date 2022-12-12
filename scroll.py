d5f0620cb3ec5785740ce223449737fe119654b1
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-12 18:01:04 +0000

    Dec 12, 2022, 9:31 PM

--- scroll.py
+++ scroll.py
@@ -114,7 +114,28 @@ with mp_hands.Hands(
                     * image_width,
                     hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y
                     * image_height,
-                )                # See if both of the wrist are in the face bounding box, zoom out.
+                )
+                # See if the index finger is in the right bezel of the screen.
+                if index_finger_tip_coords[0] > image_width * 0.75:
+                    # See if the index finger is in the top bezel of the screen.
+                    if index_finger_tip_coords[1] < image_height * 0.25:
+                        # Zoom in on the face.
+                        zoom_factor = 1.25
+                    # See if the index finger is in the bottom bezel of the screen.
+                    elif index_finger_tip_coords[1] > image_height * 0.75:
+                        # Zoom out on the face.
+                        zoom_factor = 1.1
+                # See if the index finger is in the left bezel of the screen.
+                if index_finger_tip_coords[0] < image_width * 0.25:
+                    # See if the index finger is in the top bezel of the screen.
+                    if index_finger_tip_coords[1] < image_height * 0.25:
+                        # Zoom in on the face.
+                        zoom_factor = 1.25
+                    # See if the index finger is in the bottom bezel of the screen.
+                    elif index_finger_tip_coords[1] > image_height * 0.75:
+                        # Zoom out on the face.
+                        zoom_factor = 1.1
+                # see if both of the wrist are in the face bounding box, zoom out.
                 if (
                     wrist_coords[0] > x
                     and wrist_coords[0] < x + w
