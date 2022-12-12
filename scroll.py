e442f167e11804bb93c6532813c2d0a2cec6929e
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-12 14:58:58 +0000

    Dec 12, 2022, 6:28 PM

--- scroll.py
+++ scroll.py
@@ -29,9 +29,12 @@ def text_overlay(image, text, x, y, color, font_size):
 
 # Function to zoom on the detected face and follow it.
 def zoom_in(image, x, y, w, h, zoom_factor):
-
     # Zoom factor must be greater than 1.
     zoom_factor = zoom_factor**4
+
+    # Get the dimensions of the image.
+    width, height = image.shape[1], image.shape[0]
+
     # Get the width and height of the face bounding box.
     face_width, face_height = w, h
 
@@ -41,22 +44,34 @@ def zoom_in(image, x, y, w, h, zoom_factor):
     # Calculate the starting and ending x and y coordinates of the face in the image.
     start_x, start_y, end_x, end_y = face_x - face_width / 2 * zoom_factor, face_y - face_height / 2 * zoom_factor, face_x + face_width / 2 * zoom_factor, face_y + face_height / 2 * zoom_factor
 
-    # Make sure the starting and ending x and y coordinates are within the bounds of the image. 
+    # Check if the face is near to one of the bezels.
+    is_near_bezel = (start_x <= 0 or end_x >= width or start_y <= 0 or end_y >= height)
+
+    # If the face is not near to one of the bezels, keep the frame box centered on the face.
+    if not is_near_bezel:
+        start_x, start_y, end_x, end_y = face_x - face_width / 2 * zoom_factor, face_y - face_height / 2 * zoom_factor, face_x + face_width / 2 * zoom_factor, face_y + face_height / 2 * zoom_factor
+
+    # Make sure the starting and ending x and y coordinates of the frame box are within the bounds of the image.
     start_x = max(0, start_x)
     start_y = max(0, start_y)
-    end_x = min(image.shape[1] - 1, end_x)
-    end_y = min(image.shape[0] - 1, end_y)
+    end_x = min(width - 1, end_x)
+    end_y = min(height - 1, end_y)
 
     # Get the sub-image of the face.
     face_image = image[int(start_y) : int(end_y), int(start_x) : int(end_x)]
 
     # Resize the face image to fit the original image size, keep the aspect ratio.
     face_image = cv2.resize(
-        face_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_LANCZOS4, dst=face_image )
-    image = face_image
+        face_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_LANCZOS4, dst=face_image
+    )
+
     # Overlay the face image on the original image.
-    # image = cv2.addWeighted(src1=image, alpha=0, src2=face_image, beta=1, gamma=0)
+    image = face_image
+
+    # Return the resulting image.
     return image
+
+
 
 
 # For webcam input:
