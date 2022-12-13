6bda15443f243fd9848e88043063fb5a61afec7c
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-13 08:46:07 +0000

    Dec 13, 2022, 12:16 PM

--- mouse.py
+++ mouse.py
@@ -10,9 +10,8 @@ def move_mouse(x, y):
   y_scaled = int(y * screen_height)
 
 
-  # Move the mouse to the scaled coordinates.
-  ctypes.windll.user32.SetCursorPos(-x_scaled, y_scaled)
-
+  # Move the mouse to the scaled coordinates. For mirroring the screen, swap x_scaled and screen_width - x_scaled.
+  ctypes.windll.user32.SetCursorPos(x_scaled, y_scaled)
 def click_mouse():
   # Simulate a left mouse button click.
   ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)
