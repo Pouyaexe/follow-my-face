c314d471974a269e9cb09ffada2b9db790e006f8
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-13 08:12:02 +0000

    Dec 13, 2022, 11:42 AM

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import cv2
 import mediapipe as mp
+import types
+import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
