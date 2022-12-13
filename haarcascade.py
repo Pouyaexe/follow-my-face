dd5804360dd5c0838194a0151affa4e33cedf35b
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-13 07:41:38 +0000

    Dec 13, 2022, 11:11 AM

--- haarcascade.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-
-url = 'https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml'
-urllib.request.urlretrieve(url, 'haarcascade_frontalface_default.xml')
