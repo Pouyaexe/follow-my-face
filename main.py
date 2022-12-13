e276951f415138568c9e84b9ddb0aeaa82adb84d
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-13 08:55:32 +0000

    Dec 13, 2022, 12:25 PM

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ with mp_hands.Hands(
             # Simulate a left mouse button click when the index is pointing at the camera(all of the landmarks are in the same x coordinate).
             if results.multi_hand_landmarks[0].landmark[8].x == results.multi_hand_landmarks[0].landmark[5].x == results.multi_hand_landmarks[0].landmark[9].x == results.multi_hand_landmarks[0].landmark[13].x == results.multi_hand_landmarks[0].landmark[17].x == results.multi_hand_landmarks[0].landmark[1].x:
                 click_mouse()
-                print           
+                print("Click!")           
         # if the zoom factor is greater than 1, then we are zoomed in on the face.
         if zoom_factor > 1:
             # Draw the zoomed in face on the screen.
