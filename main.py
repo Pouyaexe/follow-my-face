d4209b734e69328df5be0f45aaead851ce9b61e7
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-18 13:28:12 +0000

    Dec 18, 2022, 4:58 PM

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ with mp_hands.Hands(
             cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
         # Process the hands in the image.
-        image = process_hands(image, results)
+        image = process_hands(image, results, x, y, w, h)
                         
         # if the zoom factor is greater than 1, then we are zoomed in on the face.
         if zoom_factor > 1:
