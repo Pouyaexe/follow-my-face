8c9100f1c1153bc29801d46bdfa646b4264d7add
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-12-18 13:20:01 +0000

    Dec 18, 2022, 4:50 PM

--- face.py
+++ face.py
@@ -1,6 +1,17 @@
 import cv2
 
+# Load the Haar cascade classifier for face detection.
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 def detect_face(image):
+    """d
+
+    Args:
+        image (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     # Detect faces in the image.
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
